9ebeeda737722a298ef0ed66e37fb9fb1fa8bd1d
Oyeniyi Joseph <josoye24@gmail.com>
2023-11-20 04:21:36 +0100

    update env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { InvoiceModule } from './invoice/invoice.module';
+import { ConfigAppModule } from './config/config.module';
 
 @Module({
   imports: [
+    ConfigAppModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3001); // get port from env else use 3001
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
